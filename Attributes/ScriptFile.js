7d712fa4699dd9c4e845cf6ea231d575a5618ca3
Bharath-Naidu <nbharat229@gmail.com>
2018-09-19 19:36:23 +0530

    jQuery

---
+++ Attributes/ScriptFile.js
@@ -0,0 +1,54 @@
+﻿$(document).ready(function () {
+
+
+    //$("#p-tag").attr("class", "p-class");
+
+    $("#p-tag").click(callme);  
+
+    function callme()
+    {
+        $("#p-tag").attr("class", "p-class");
+        //$("#p-tag").attr("text", "changes here");
+    }
+
+    
+    $("#divid").click(function ()
+    {
+        $("#divnid").fadeToggle("slow");
+       // $("#divnid").slideDown();
+        //$("#divnid").slideToggle("slow");
+    });
+
+    $("#button-id").click(function ()
+    {
+        alert("button is clicked");
+    });
+
+    $(".txtfield").mouseenter(function () {
+        alert("please enter your name");
+        callmehere();
+    });
+
+    $("#St").mouseenter(function () {
+        //alert("enter");
+        $("#St").prop("style", "white");
+    });
+
+    var val1 = $("#First-name").val();
+    var val2 = $("#Second-name").val();
+    $("#Display-button").click(function () {
+        //alert("here");
+        if (($("#First-name").val().length === 0) && ($("#Second-name").val().length === 0)) {
+            console.log($("#First-name").val().length);
+            console.log($("#Second-name").val().length);
+            alert("empty field");
+        }
+    });
+
+    //function callmehere() {
+        
+    //    //$("#textid").unbind("class");
+    //    $("#textid").removeClass("txtfield");
+    //    alert("its comes");
+    //}
+});
---
+++ WEB_FOAM/Scripting.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function ()
+{
+    
+    $("#Display-button").click(function ()
+    {
+        var val1 = $("#First-name").val();
+        var val2 = $("#Second-name").val();
+        //alert("here");
+        if ((val1 == "") || (val2==""))
+        {
+            console.log($("#First-name").val().length);
+            console.log($("#Second-name").val().length);
+            alert("empty field");
+        } 
+    });
+    
+});
